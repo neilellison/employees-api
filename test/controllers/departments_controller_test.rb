03059a330e6c41374e21e6a97241cb2eeb80300b
NeilEllison <ellison.neil1@gmail.com>
2023-03-14 12:36:04 -0700

    added department tests

--- test/controllers/departments_controller_test.rb
+++ test/controllers/departments_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class DepartmentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/departments.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Department.count, data.length
+  end
+
+  test "show" do
+    get "/departments/#{Department.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "manager", "code", "employees"], data.keys
+  end
+
+  test "create" do
+    assert_difference "Department.count", 1 do 
+      post "/departments.json", params: {name: "test_name", manager: "test_manager", code: 1}
+      data = JSON.parse(response.body)
+      assert_response 201
+    end  
+  end  
 end
