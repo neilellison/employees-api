af8c25f0e414764bb50dd94040f29ab111c53e23
NeilEllison <ellison.neil1@gmail.com>
2023-03-14 13:21:34 -0700

    added update and destroy tests

--- test/controllers/departments_controller_test.rb
+++ test/controllers/departments_controller_test.rb
@@ -24,4 +24,18 @@ class DepartmentsControllerTest < ActionDispatch::IntegrationTest
       assert_response 201
     end  
   end  
+
+  test "update" do
+    patch "/departments/#{Department.first.id}.json", params: {name: "updated department"}
+    data = JSON.parse(response.body)
+    assert_response 200
+    assert_equal "updated department", data["name"]
+  end  
+
+  test "destroy" do
+    assert_difference "Department.count", -1 do
+      delete "/departments/#{Department.first.id}.json"
+      assert_response 200
+    end
+  end  
 end
